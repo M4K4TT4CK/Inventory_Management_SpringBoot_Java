6c9e3e4d2353d4179701fc554f7342bcfe95228c
M4K4TT4CK <mikemcmillin@gmail.com>
2023-10-11 10:35:01 -0400

    Self test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+	//notes for self-test
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
